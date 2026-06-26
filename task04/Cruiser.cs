5a60ddb13f8c050ef6926118430ef6834c58bec2
noLovv47 <epidemiev@gmail.com>
2026-06-26 21:50:07 +0600

    task04

---
+++ task04/Cruiser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task04
+{
+    public class Cruiser : ISpaceship
+    {
+        public int Speed => 50;
+        public int FirePower => 100;
+        private int _angle = 0;
+
+        public void MoveForward()
+        {
+            Console.WriteLine($"Крейсер движется вперёд со скоростью {Speed} км/ч");
+        }
+
+        public void Rotate(int angle)
+        {
+            _angle = (_angle + angle) % 360;
+            Console.WriteLine($"Крейсер повернут на угол {_angle} градусов");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine($"Крейсер выпускает мощную фотонную ракету! Мощность {FirePower} МВт");
+        }
+    }
+}
---
+++ task04/Fighter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task04
+{
+    public class Fighter : ISpaceship
+    {
+        public int Speed => 100;
+        public int FirePower => 50;
+        private int _angle = 0;
+
+        public void MoveForward()
+        {
+            Console.WriteLine($"Истребитель летит вперёд со скоростью {Speed} км/ч");
+        }
+
+        public void Rotate(int angle)
+        {
+            _angle = (_angle + angle) % 360;
+            Console.WriteLine($"Истребитель повернут на угол {_angle} градусов");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine($"Истребитель выпускает быструю, но слабую ракету. Мощность {FirePower} МВт");
+        }
+    }
+}
