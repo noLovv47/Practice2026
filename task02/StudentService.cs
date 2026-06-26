e02b6dd0149a89baf3a21c7d3d89ae7eb64fe8b5
noLovv47 <epidemiev@gmail.com>
2026-06-26 15:41:20 +0600

    task02

---
+++ task02/StudentService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task02
+{
+    public class StudentService
+    {
+        private readonly List<Student> _students;
+
+        public StudentService(List<Student> students)
+        {
+            _students = students ?? throw new ArgumentNullException(nameof(students));
+        }
+
+        public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+        {
+            return _students.Where(s => s.Faculty == faculty);
+        }
+
+        public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade)
+        {
+            return _students.Where(s => s.Grades.Average() >= minAverageGrade);
+        }
+
+        public IEnumerable<Student> GetStudentsOrderedByName()
+        {
+            return _students.OrderBy(s => s.Name);
+        }
+
+        public ILookup<string, Student> GroupStudentsByFaculty()
+        {
+            return _students.ToLookup(s => s.Faculty);
+        }
+
+        public string GetFacultyWithHighestAverageGrade()
+        {
+            return _students
+                .GroupBy(s => s.Faculty)
+                .Select(g => new
+                {
+                    Faculty = g.Key,
+                    Average = g.Average(s => s.Grades.Average())
+                })
+                .OrderByDescending(x => x.Average)
+                .Select(x => x.Faculty)
+                .FirstOrDefault();
+        }
+    }
+}
