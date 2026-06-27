0bfde7b8c922c360cfc1a37990c644cbfe4514f5
noLovv47 <epidemiev@gmail.com>
2026-06-27 16:11:08 +0600

    task05

---
+++ task05/ClassAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace task05
+{
+    public class ClassAnalyzer
+    {
+        private readonly Type _type;
+
+        public ClassAnalyzer(Type type)
+        {
+            _type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+
+        public IEnumerable<string> GetPublicMethods()
+        {
+            return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+                        .Select(m => m.Name)
+                        .ToList();
+        }
+
+        public IEnumerable<string> GetMethodParams(string methodName)
+        {
+            if (string.IsNullOrEmpty(methodName))
+                return new List<string>();
+
+            var method = _type.GetMethod(methodName, BindingFlags.Public | BindingFlags.Instance);
+
+            if (method == null)
+                return new List<string>();
+
+            var result = new List<string>();
+
+            result.Add($"Return: {method.ReturnType.Name}");
+
+            var parameters = method.GetParameters();
+            if (parameters.Any())
+            {
+                result.AddRange(parameters.Select(p => $"{p.Name}: {p.ParameterType.Name}"));
+            }
+
+            return result;
+        }
+
+        public IEnumerable<string> GetAllFields()
+        {
+            return _type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                        .Select(f => f.Name)
+                        .ToList();
+        }
+
+        public IEnumerable<string> GetProperties()
+        {
+            return _type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .Select(p => p.Name)
+                        .ToList();
+        }
+
+        public bool HasAttribute<T>() where T : Attribute
+        {
+            return _type.GetCustomAttributes(typeof(T), false).Any();
+        }
+    }
+}
---
+++ task05tests/ClassAnalyzerTests.cs
@@ -0,0 +1,90 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using task05;
+
+namespace task05tests
+{
+    public class TestClass
+    {
+        public int PublicField;
+        private string _privateField;
+        public int Property { get; set; }
+
+        public void Method() { }
+        public int MethodWithParams(int a, string b) => a;
+        private void PrivateMethod() { }
+    }
+
+    [Serializable]
+    public class AttributedClass { }
+
+    public class ClassAnalyzerTests
+    {
+        [Fact]
+        public void GetPublicMethods_ReturnsCorrectMethods()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var methods = analyzer.GetPublicMethods().ToList();
+
+            Assert.Contains("Method", methods);
+            Assert.Contains("MethodWithParams", methods);
+            Assert.DoesNotContain("PrivateMethod", methods);
+        }
+
+        [Fact]
+        public void GetAllFields_IncludesPrivateFields()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var fields = analyzer.GetAllFields().ToList();
+
+            Assert.Contains("PublicField", fields);
+            Assert.Contains("_privateField", fields);
+        }
+
+        [Fact]
+        public void GetProperties_ReturnsCorrectProperties()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var properties = analyzer.GetProperties().ToList();
+
+            Assert.Contains("Property", properties);
+        }
+
+        [Fact]
+        public void GetMethodParams_ReturnsCorrectParameters()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var paramsInfo = analyzer.GetMethodParams("MethodWithParams").ToList();
+
+            Assert.Equal(3, paramsInfo.Count);
+            Assert.Contains("Return: Int32", paramsInfo);
+            Assert.Contains("a: Int32", paramsInfo);
+            Assert.Contains("b: String", paramsInfo);
+        }
+
+        [Fact]
+        public void GetMethodParams_InvalidMethod_ReturnsEmpty()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var paramsInfo = analyzer.GetMethodParams("NonExistentMethod");
+            Assert.Empty(paramsInfo);
+        }
+
+        [Fact]
+        public void HasAttribute_WithAttribute_ReturnsTrue()
+        {
+            var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+            var result = analyzer.HasAttribute<SerializableAttribute>();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void HasAttribute_WithoutAttribute_ReturnsFalse()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var result = analyzer.HasAttribute<SerializableAttribute>();
+            Assert.False(result);
+        }
+    }
+}
